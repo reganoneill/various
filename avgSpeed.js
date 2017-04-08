0005580bfa4a39a7ed29293253e054a12fc0b797
reganoneill <rkrotho@gmail.com>
2017-04-07 23:27:00 -0700

    problem domain

---
+++ avgSpeed.js
@@ -0,0 +1,15 @@
+'use strict';
+//https://www.codewars.com/kata/56484848ba95170a8000004d
+
+// distances traveled (dataset)[0.0, 0.19, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25]
+//s = seconds between distance checks
+const s = 15;
+var x = [45.6, 74.4, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0, 60.0];
+
+x.reduce(function(acc, cur){
+  var highestAvg = acc;
+  if(cur > highestAvg){
+    highestAvg = cur;
+  }
+  return Math.floor(highestAvg);
+});
